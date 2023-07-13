5c67e6c358ca56c79b5f92c37156203d8d4051da
ahsanaaxioned <ahsana@axioned.com>
2023-07-14 01:14:30 +0530

    logic updated for components reusabilty

--- src/components/FlashCard.jsx
+++ src/components/FlashCard.jsx
@@ -4,7 +4,7 @@ import CardData from "../data/Data";
 const FlashCard = () => {
   const [cardDetails] = useState(CardData);
   const [index, setIndex] = useState(0);
-  // console.log(cardDetails);
+
   useEffect(() => {
     if (index < 0) {
       setIndex(CardData.length - 1);
@@ -12,38 +12,36 @@ const FlashCard = () => {
     if (index > CardData.length - 1) {
       setIndex(0);
     }
-  }, [index, cardDetails]);
-  useEffect(() => {
-    let slideCard = setInterval(() => {
-      setIndex(index + 1);
-    }, 5000);
-    console.log(index);
-    // return clearInterval(slideCard);
   }, [index]);
+
   return (
-    <div className="main-container">
-    <h1 className='heading'>React Flashcard</h1>
-    <div className = 'card-container'>
-     {cardDetails.map((item,cardIndex)=>{
-      // console.log(item)
-       const {id,heading} = item;
-       let position = 'next-slide';
-       if(cardIndex === index){
-        console.log('cardIndex:',cardIndex);
-        console.log('index:',index);
-        position = 'activeSlide'
-       }
-      //  console.log(id,heading);
-       return(
-        <div key = {id} className={`card-detail  ${position}`}>
-       <span>{id}</span>
-       <h2>{heading}</h2>
-       </div>
-       )
-     })}
-    </div>
-    </div>
-  )
+    <>
+      <h1 className="heading">React Flashcard</h1>
+      <ul className="card-container">
+        {cardDetails.map((item, cardIndex) => {
+          const { id, heading } = item;
+          let position = false ;
+          if (cardIndex === index) {
+            // console.log("cardIndex:", cardIndex);
+            // console.log("index:", index);
+            position = true;
+          }
+          return (
+            <li key={id} className={`card-detail  ${position ? 'active-slide' : ''}`}>
+              <span>{id}</span>
+              <h2>{heading}</h2>
+            </li>
+          );
+        })}
+      </ul>
+      <button className="btn" onClick={() => setIndex(index -1)} >
+        previous
+      </button>
+      <button className="btn" onClick={() => setIndex(index + 1)} >
+      Next
+      </button>
+      </>
+  );
 };
 
 export default FlashCard;
