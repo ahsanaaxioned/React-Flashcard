f107bf2890e74c86bcb1f102d9f7d82e8b0816d7
ahsanaaxioned <ahsana@axioned.com>
2023-07-14 12:15:21 +0530

    final logic updated

--- src/components/FlashCard.jsx
+++ src/components/FlashCard.jsx
@@ -1,46 +1,30 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import CardData from "../data/Data";
 
 const FlashCard = () => {
   const [cardDetails] = useState(CardData);
   const [index, setIndex] = useState(0);
 
-  useEffect(() => {
-    if (index < 0) {
-      setIndex(CardData.length - 1);
-    }
-    if (index > CardData.length - 1) {
-      setIndex(0);
-    }
-  }, [index]);
+  const increment = () =>
+    index >= CardData.length - 1 ? setIndex(0) : setIndex(index + 1);
+
+  const decrement = () =>
+    index <= 0 ? setIndex(CardData.length - 1) : setIndex(index - 1);
 
   return (
     <>
       <h1 className="heading">React Flashcard</h1>
-      <ul className="card-container">
-        {cardDetails.map((item, cardIndex) => {
-          const { id, heading } = item;
-          let position = false ;
-          if (cardIndex === index) {
-            // console.log("cardIndex:", cardIndex);
-            // console.log("index:", index);
-            position = true;
-          }
-          return (
-            <li key={id} className={`card-detail  ${position ? 'active-slide' : ''}`}>
-              <span>{id}</span>
-              <h2>{heading}</h2>
-            </li>
-          );
-        })}
-      </ul>
-      <button className="btn" onClick={() => setIndex(index -1)} >
+      <div className="card-container">
+        <span>{cardDetails[index].id}</span>
+        <h2>{cardDetails[index].heading}</h2>
+      </div>
+      <button className="btn" onClick={() => decrement()}>
         previous
       </button>
-      <button className="btn" onClick={() => setIndex(index + 1)} >
-      Next
+      <button className="btn" onClick={() => increment()}>
+        Next
       </button>
-      </>
+    </>
   );
 };
 
